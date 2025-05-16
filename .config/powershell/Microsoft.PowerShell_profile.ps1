5d80be0b6304caf439d40b676b4c778bc341613f
Michael Cho <michael88cho@gmail.com>
2025-05-17 00:07:18 +1000

    Updated Kitty and nvim

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,17 @@
+$profileStartTime = Get-Date
+
 oh-my-posh init pwsh | Invoke-Expression
+$poshEndTime = Get-Date
+$loadDuration = $poshEndTime - $profileStartTime
+
+Write-Host "✅ PowerShell OhMyPosh loaded in $($loadDuration.TotalMilliseconds) ms" -ForegroundColor Green
+
 function dotfiles {
     git --git-dir=$HOME/.dotfiles --work-tree=$HOME @Args
 }
 #New-Alias dotfiles dotfiles
+#. ".$HOME\Git\vms-bob\bob" 
 
-Write-Host "✅ PowerShell profile loaded!"
-
+$profileEndTime = Get-Date
+$loadDuration = $profileEndTime - $profileStartTime
+Write-Host "✅ PowerShell profile loaded in $($loadDuration.TotalMilliseconds) ms" -ForegroundColor Green
